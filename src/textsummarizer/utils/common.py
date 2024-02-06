21f2f6853e5bfdbe1d22270b37cd0a048996a67f
yatin kundra <ykundra44@gmail.com>
2024-02-06 12:48:35 +0530

    installed requirenments.txt

---
+++ src/textsummarizer/utils/common.py
@@ -0,0 +1,5 @@
+# the function that we are going to be using throughout our code , that are peresnt in uitls/common.py
+# instead of writng them again and again we write them once here and then call them when ever we need them
+
+import os
+from box import Box
